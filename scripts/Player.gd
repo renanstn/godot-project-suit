63e3f0826afc22b65bbc2b75324792914ba369e9
Renan Santana <renan_ota@hotmail.com>
2019-02-07 00:42:54 -0200

    Personagem agora Atira no angulo mirado

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,31 +4,30 @@ const UP = Vector2(0, -1)
 const GRAVITY = 12
 const SPEED = 200
 const JUMP = 500
+const BULLET_SPEED = 5
 var motion = Vector2()
-var localMousePosition
-var globalMousePosition
 var lookRight
 var playerPosition
+var bullet = preload("res://scenes/Shoot.tscn")
 
 func _physics_process(delta):
 	
-	# Get local and global mouse positions
-	localMousePosition  = get_local_mouse_position()
-	globalMousePosition = get_global_mouse_position()
-	
 	# Fazer o braço apontar para o mouse
-	$Arm.rotation = localMousePosition.angle()
+	$Arm.rotation = get_local_mouse_position().angle()
 	
 	# Checar se o player esta olhando pra esquerda ou direita
-	if position.x < globalMousePosition.x:
+	if position.x < get_global_mouse_position().x:
 		lookRight = 1
 	else:
 		lookRight = 0
 		
-#	if lookRight:
-#		set_scale(Vector2(1, 1))
-#	else:
-#		set_scale(Vector2(-1, 1))
+	# Flip nos sprites quando o personagem vira
+	if lookRight:
+		$Sprite.set_scale(Vector2(1, 1))
+		$Arm.set_scale(Vector2(1.5, 0.5))
+	else:
+		$Sprite.set_scale(Vector2(-1, 1))
+		$Arm.set_scale(Vector2(1.5, -0.5))
 	
 	# Gravity
 	motion.y += GRAVITY
@@ -36,17 +35,9 @@ func _physics_process(delta):
 	# Movement
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
-		# Inverter o eixo caso seja um sprite simples ou pré animado
-		$Sprite.flip_h = false
-		# Utilizar este outro método de inversão caso seja um sprite com várias partes montadas em uma animação estilo cutout
-		# $Sprite.set_scale(Vector2(1,1))
 		# $Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
-		# Inverter o eixo caso seja um sprite simples ou pré animado
-		$Sprite.flip_h = true
-		# Utilizar este outro método de inversão caso seja um sprite com várias partes montadas em uma animação estilo cutout
-		# $Sprite.set_scale(Vector2(-1,1))
 		# $Sprite.play("run")
 	else:
 		motion.x = 0
@@ -65,8 +56,11 @@ func _physics_process(delta):
 	motion = move_and_slide(motion, UP)
 	
 func shoot():
-	var shoot = load("res://scenes/Shoot.tscn").instance()
-	shoot.position = $Arm/GunPoint.get_global_position()
-	# shoot.add_impulse(Vector2(0, 0), Vector2(50, 0))
-	get_parent().add_child(shoot)
-	
+	var clone_bullet = bullet.instance()
+	# var bullet_angle = get_angle_to(get_global_mouse_position()) + $Arm/SpawnBullets.get_rotation()
+	# clone_bullet.set_rotation(bullet_angle)
+	clone_bullet.rotation = $Arm.rotation
+	clone_bullet.position = $Arm/SpawnBullets.get_global_position()
+	get_parent().add_child(clone_bullet)
+	var direction = get_global_mouse_position() - get_global_position()
+	clone_bullet.apply_impulse(Vector2(), direction * BULLET_SPEED)
